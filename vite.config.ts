6cd24688e4aa6ce6ba2fac25f03f50adf4916124
haramjeong <04harams77@gmail.com>
2026-05-12 13:27:00 +0900

    feat: vite.config.ts 환경변수 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 4173,
-    proxy: {
-      // 핵심: 브라우저는 4173에 요청을 보내는 줄 알지만, Vite 서버가 중간에서 낚아챕니다.
-      "/api": {
-        target: "https://return-api.khu-return.com",
-        changeOrigin: true,
-        secure: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, './'); 
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_RCP_API_BASE_URL || 'https://return-api.khu-return.com',
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
